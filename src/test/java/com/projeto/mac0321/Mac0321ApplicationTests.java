1e913de3241e0a64d63bce4a2f7e34e671e1fb79
mac0321 07 <mac0321.07@fred.cec.ime.usp.br>
2022-04-28 10:14:45 -0300

    initial spring boot setup

---
+++ src/test/java/com/projeto/mac0321/Mac0321ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.mac0321;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mac0321ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
