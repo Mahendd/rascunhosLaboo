68d35c4b2d4eed321a0ec7a6ff67139794aa419f
rodrigosalesmarcolin <rodrigosalesmarcolin@gmail.com>
2022-05-04 22:32:28 -0300

    playlist prototypes

--- src/frontend/src/components/Playlist.jsx
+++ src/frontend/src/components/Playlist.jsx
@@ -1,9 +1,11 @@
 import Avatar from "@mui/material/Avatar"
 import styles from "./Playlist.module.css"
 
+// A playlist recebe uma URL de avatar
+// um título
 export default function Playlist(props) {
     return (
-      <div className={styles.playlist}>
+      <div className={[styles.playlist, styles.fade_in].join(" ")}>
         <div className={styles.avatarRegion}>
           <Avatar alt="Teste" className={styles.avatar} src={props.avatar} />
         </div>
--- src/frontend/src/pages/playlists.jsx
+++ src/frontend/src/pages/playlists.jsx
@@ -3,15 +3,24 @@ import Header from "../components/Header"
 import Main from "../components/Main"
 import Footer from "../components/Footer"
 import Playlist from "../components/Playlist"
+import styles from "../styles/Playlists.module.css"
 export default function Playlists() {
     return (
         <BodyGrid>
           <Header>
             <h1>Minhas Playlists</h1>
           </Header>
-          <Main>
-            <Playlist />
-          </Main>
+          <main className={styles.main}>
+            <Playlist title="Samba Boêmio" avatar={avatar(50)} />
+            <Playlist title="Rock Pesadão" avatar={avatar(180)}/>
+            <Playlist title="Para chorar no banho" avatar={avatar(6969)}/>
+            <Playlist title="Baguncinha com os amigos" avatar={avatar(4242)}/>
+            
+          </main>
         </BodyGrid>
     )
+}
+
+function avatar(seed) {
+  return "https://avatars.dicebear.com/api/identicon/" + seed+".svg"
 }
