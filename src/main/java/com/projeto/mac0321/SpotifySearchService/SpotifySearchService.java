0be189a1af8b503797523e7a006df9934f9ba8cb
rTaiShan <raul.tai@usp.br>
2022-05-10 22:05:03 -0300

    Added SpotifySearchService with incomplete authentication.

---
+++ src/main/java/com/projeto/mac0321/SpotifySearchService/SpotifySearchService.java
@@ -0,0 +1,33 @@
+package com.projeto.mac0321.SpotifySearchService;
+
+import org.apache.hc.core5.http.ParseException;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.special.SearchResult;
+import se.michaelthelin.spotify.requests.data.search.SearchItemRequest;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class SpotifySearchService {
+    private final SpotifyApi spotifyApi;
+
+    public SpotifySearchService(String clientId, String clientSecret, URI redirectURI) {
+        spotifyApi = new SpotifyApi.Builder()
+                .setClientId(clientId)
+                .setClientSecret(clientSecret)
+                .setRedirectUri(redirectURI)
+                .build();
+    }
+
+    SearchResult searchItem(String searchTerm, String type) {
+        final SearchItemRequest searchItemRequest = spotifyApi.searchItem(searchTerm, type)
+                .build();
+
+        try {
+            return searchItemRequest.execute();
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            return null;
+        }
+    }
+}
